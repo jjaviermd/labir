77e5ebbc68e7351deb56b4999a02cb36ddeb3605
jjavier <razornice@gmail.com>
2023-09-11 12:14:47 -0300

    pages conmtroller and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/settings', to: 'pages#settings'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
 end
