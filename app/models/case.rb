1e2d4a69d82e64b4a5b187a1bd510b37df14d15c
jjavier <razornice@gmail.com>
2023-09-13 21:50:19 -0300

    case and pathogist validation

--- app/models/case.rb
+++ app/models/case.rb
@@ -3,4 +3,13 @@
 class Case < ApplicationRecord
   belongs_to :patient
   belongs_to :pathologist
+  enum status: { gross_examination: 1,
+                 histotechnology: 2,
+                 microscopic_examination: 3,
+                 diagnosed: 4 }
+  enum type: { biopsy: 1, citology: 2, autopsy: 3 }
+  validates :status, numericality: { only_integer: true, in: 1..4 }
+  validates :type, numericality: { only_integer: true, in: 1..3 }
+  validates :protocol_number, uniqueness: true
+  validates :organ, presence: true
 end
--- app/models/pathologist.rb
+++ app/models/pathologist.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Pathologist < ApplicationRecord
+  validates :name, :last_name, presence: true
 end
