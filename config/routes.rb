cf3d36790c08edd5b2a4d1ed6ea40124b0904d01
jjavier <razornice@gmail.com>
2023-09-20 01:43:52 -0300

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/settings'
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
   get '/settings', to: 'pages#settings'
