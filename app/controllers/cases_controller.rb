3fe708094ef501a9bd0b12d6141b4d4e95683908
jjavier <razornice@gmail.com>
2023-10-03 15:38:07 -0300

    new case for non-existing patient plus: new flash partial

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -15,9 +15,9 @@ class CasesController < ApplicationController
     @case = @patient.cases.build case_params
     if @case.save
       redirect_to case_path(@case)
-      flash[:notice] = "#{@patient.full_name}`s new case have been created. Protocol number #{@case.protocol_number}"
+      flash[:success] = "#{@patient.full_name}`s new case have been created. Protocol number #{@case.protocol_number}."
     else
-      flash.now[:notice] = 'Something went wrong and case was not created'
+      flash.now[:danger] = 'Something went wrong and case was not created.'
       render :new, status: :unprocessable_entity
     end
   end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -9,13 +9,16 @@ class PatientsController < ApplicationController
 
   def new
     @patient = Patient.new
+    @case = @patient.cases.build
   end
 
   def create
     @patient = Patient.new(patient_params)
     if @patient.save
       redirect_to patient_path(@patient)
+      flash[:success] = "#{@patient.full_name}`s new case have been created. Protocol number #{@patient.cases.last.protocol_number}."
     else
+      flash.now[:danger] = 'Something went wrong, patient and case were not created.'
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,7 +32,20 @@ class PatientsController < ApplicationController
   def patient_params
     params.require(:patient).permit(:dni, :f_last_name, :l_last_name, :name,
                                     :phone_number, :email, :insurance, :age,
-                                    :birth_day, :gender)
+                                    :birth_day, :gender,
+                                    cases_attributes: %i[
+                                      pathologist_id
+                                      status
+                                      macro_description
+                                      micro_description
+                                      diagnosis
+                                      organ
+                                      physician
+                                      speciality
+                                      protocol_number
+                                      notes
+                                      type_of_sample
+                                    ])
   end
 
   def set_patient
