e9f4376ca2f2b4d5ae03ce51c5a045052a020e52
jjavier <razornice@gmail.com>
2023-10-02 22:52:13 -0300

    new case for existing patient

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -6,10 +6,21 @@ class CasesController < ApplicationController
   def show; end
 
   def new
-    @case = Case.new
+    @patient = Patient.find params[:patient_id]
+    @case = @patient.cases.build
   end
 
-  def create; end
+  def create
+    @patient = Patient.find case_params[:patient_id]
+    @case = @patient.cases.build case_params
+    if @case.save
+      redirect_to case_path(@case)
+      flash[:notice] = "#{@patient.full_name}`s new case have been created. Protocol number #{@case.protocol_number}"
+    else
+      flash.now[:notice] = 'Something went wrong and case was not created'
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def edit
     @patient = @case.patient
@@ -21,5 +32,11 @@ class CasesController < ApplicationController
 
   def set_case
     @case = Case.find params[:id]
+  end
+
+  def case_params
+    params.require(:case).permit(:patient_id, :pathologist_id, :status, :macro_description,
+                                 :micro_description, :diagnosis, :organ, :physician,
+                                 :speciality, :protocol_number, :notes, :type_of_sample)
   end
 end
