c7a11514bbcd8aa1437b6b3c0dcc5c96914a488c
jjavier <razornice@gmail.com>
2023-10-23 14:18:23 -0300

    _mpty_state home cases

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+puts "\n== Deleting data =="
 Case.delete_all
+puts "\n== cases deleted =="
 Patient.delete_all
+puts "\n== Patients deleted =="
 Pathologist.delete_all
-
-puts "\n== Seeding the database with fixtures =="
-system('bin/rails db:fixtures:load')
+puts "\n== Pathologists deleted =="
+puts "\n== data deleted =="
+# puts "\n== Seeding the database with fixtures =="
+# system('bin/rails db:fixtures:load')
