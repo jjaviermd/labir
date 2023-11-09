8081759cd20b1b3d3775cefd756355ff8f9756e3
jjavier <razornice@gmail.com>
2023-11-09 15:06:10 -0300

    devise set to sign in with account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_laboratory!
   # protect_from_forgery with: :exception
 
   # before_action :update_allowed_parameters, if: :devise_controller?
