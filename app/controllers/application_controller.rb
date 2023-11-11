49e6ba06aba380f2cf52e2581f87cbd2c0af8dae
jjavier <razornice@gmail.com>
2023-11-11 12:09:54 -0300

    ecrypt lab password fixture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_laboratory!
+  before_action :authenticate_laboratory!, unless: :devise_controller?
   # protect_from_forgery with: :exception
 
   # before_action :update_allowed_parameters, if: :devise_controller?
