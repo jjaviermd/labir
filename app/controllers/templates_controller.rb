bb6b90d64c2f70602164866aadaf28cba43bc8b7
jjavier <razornice@gmail.com>
2023-11-24 14:35:24 -0300

    _template_form partial created

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -6,9 +6,16 @@ class TemplatesController < ApplicationController
   end
 
   def new
+    @template = current_laboratory.templates.build
   end
 
   def create
+    @template = current_laboratory.templates.build template_params
+    if @template.save
+      flash[:success] = "#{@template.name} template  created."
+    else
+      flash.now[:danger] = "#{@template.name} template not created."
+    end
   end
 
   def edit
@@ -18,5 +25,11 @@ class TemplatesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def template_params
+    params.require(:template).permit(:type, :prefix, :name, :text)
   end
 end
