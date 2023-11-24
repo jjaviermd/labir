ed7216d40015acb0635d31efe1089e3541431d5d
jjavier <razornice@gmail.com>
2023-11-24 13:53:09 -0300

    templates controller and views

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,5 @@
 class Template < ApplicationRecord
   belongs_to :laboratory
+  validates :name, :prefix, :content, presence: true
+  validates :prefix, uniqueness: {scope: [:laboratory_id, :prefix], message: "Prefix already in use"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,20 @@
 
 Rails.application.routes.draw do
   devise_for :laboratories, controllers: {
-    sessions: 'laboratories/sessions',
-    registrations: 'laboratories/registrations'
+    sessions: "laboratories/sessions",
+    registrations: "laboratories/registrations"
   }
   resources :patients, except: :destroy
   resources :cases, except: :destroy do
-    get 'sign_inform', on: :member
+    get "sign_inform", on: :member
   end
   resources :pathologists
-  get '/pathologists/:id/pending', to: 'pathologists#pending', as: 'pending'
-  get '/pathologists/:id/finished', to: 'pathologists#finished', as: 'finished'
-  get '/home', to: 'pages#home'
-  get '/about', to: 'pages#about'
-  get '/settings', to: 'pages#settings'
+  get "/pathologists/:id/pending", to: "pathologists#pending", as: "pending"
+  get "/pathologists/:id/finished", to: "pathologists#finished", as: "finished"
+  get "/home", to: "pages#home"
+  get "/about", to: "pages#about"
+  get "/settings", to: "pages#settings"
+  resources :templates
 
-  root 'pages#home'
+  root "pages#home"
 end
