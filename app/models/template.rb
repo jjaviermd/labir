a28b354384ae21e5b1fee61b344a38e2239e3f54
jjavier <razornice@gmail.com>
2023-11-28 00:09:58 -0300

    max chars validation for templates changed

--- app/models/template.rb
+++ app/models/template.rb
@@ -3,7 +3,4 @@ class Template < ApplicationRecord
   validates :prefix, length: {maximum: 10}
   validates :name, :prefix, :text, presence: true
   validates :prefix, uniqueness: {scope: [:laboratory_id, :type], message: "already in use"}
-  before_save do
-    prefix.concat "_"
-  end
 end
