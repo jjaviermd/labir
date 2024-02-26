a74c17cadd02173225f91e254ede8dd7f1e7bfe4
jjavier <razornice@gmail.com>
2024-02-25 21:21:05 -0300

    sample_received_mail mailer action sends email

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -1,7 +1,8 @@
 class PatientMailer < ApplicationMailer
   def sample_received_email
-    @case = current_laboratory.cases.find params[:id]
-    @patient = @case.patient
+    # @case = current_laboratory.cases.find params[:id]
+    @case = params[:case]
+    @patient = params[:patient]
 
     mail(to: @patient.email, subject: "Sample received")
   end
