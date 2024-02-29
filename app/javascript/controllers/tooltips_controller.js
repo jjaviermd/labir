8a5554e5b718627cd2637ab72c4db59dae96fd70
jjavier <razornice@gmail.com>
2024-02-29 15:16:33 -0300

    sendreportTooltip action

--- app/javascript/controllers/tooltips_controller.js
+++ app/javascript/controllers/tooltips_controller.js
@@ -2,10 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tooltips"
 export default class extends Controller {
+  static targets = ["tooltipText"]
   // connect() {
   //   console.log("Hello, Stimulus!", this.element)
   // }
   sendReportTooltip() {
     console.log("tooltip");
+    this.tooltipTextTarget.classList.toggle("is-hidden")
+    window.setTimeout(() => { this.tooltipTextTarget.classList.toggle("is-hidden") }, 1500)
   }
 }
