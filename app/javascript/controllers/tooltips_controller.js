d0ce15adf8b12c68b41932e37d24810646d73938
jjavier <razornice@gmail.com>
2024-02-29 16:56:47 -0300

    sendreport button is danger when disabled

--- app/javascript/controllers/tooltips_controller.js
+++ app/javascript/controllers/tooltips_controller.js
@@ -3,9 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="tooltips"
 export default class extends Controller {
   static targets = ["tooltipText"]
-  // connect() {
-  //   console.log("Hello, Stimulus!", this.element)
-  // }
+  connect() {
+    const isButtonDisabled = !!this.tooltipTextTarget.previousElementSibling.getAttribute("disabled")
+    if (isButtonDisabled) {
+      this.tooltipTextTarget.previousElementSibling.classList.toggle("is-info")
+      this.tooltipTextTarget.previousElementSibling.classList.toggle("is-danger")
+    }
+  }
   sendReportTooltip() {
     const isButtonDisabled = !!this.tooltipTextTarget.previousElementSibling.getAttribute("disabled")
     if (isButtonDisabled) {
