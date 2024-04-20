deeb864a5af909799d707bcc5bf118749aeca237
jjavier <razornice@gmail.com>
2024-04-20 10:29:08 -0400

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_24_143640) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_033341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,9 +90,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_24_143640) do
     t.string "registry_number", null: false
     t.integer "cases_count", default: 0, null: false
     t.bigint "laboratory_id", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_pathologists_on_email", unique: true
     t.index ["laboratory_id"], name: "index_pathologists_on_laboratory_id"
     t.index ["last_name"], name: "index_pathologists_on_last_name"
     t.index ["registry_number"], name: "index_pathologists_on_registry_number"
+    t.index ["reset_password_token"], name: "index_pathologists_on_reset_password_token", unique: true
   end
 
   create_table "patients", force: :cascade do |t|
