85d8203b055221026ec550dd506698a856008b2a
jjavier <razornice@gmail.com>
2024-04-20 11:45:37 -0400

    accessible concern

---
+++ app/controllers/concerns/accessible.rb
@@ -0,0 +1,19 @@
+module Accessible
+  extend ActiveSupport::Concern
+  included do
+    before_action :check_pathologist
+  end
+
+  protected
+  def check_pathologist
+    if current_laboratory
+      flash.clear
+      # if you have rails_admin. You can redirect anywhere really
+      redirect_to(welcome_path) and return
+    elsif current_pathologist
+      flash.clear
+      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+      redirect_to(welcome_path_path) and return
+    end
+  end
+end
