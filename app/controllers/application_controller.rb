25055d016aa955dd30fc830fa9e373847be87054
jjavier <razornice@gmail.com>
2024-04-22 13:54:49 -0400

    both devise model can signin and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_laboratory!, unless: :devise_controller?
+  # before_action :authenticate_laboratory!, unless: :devise_controller?
 
   before_action :set_current_laboratory
 
--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -9,11 +9,11 @@ module Accessible
     if current_laboratory
       flash.clear
       # if you have rails_admin. You can redirect anywhere really
-      redirect_to(welcome_path) and return
+      redirect_to(home_path) and return
     elsif current_pathologist
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(welcome_path_path) and return
+      redirect_to(home_path) and return
     end
   end
 end
