19f8f443d8ad6ac98b418e9eb72cb1e205875430
jjavier <razornice@gmail.com>
2024-05-06 19:42:19 -0400

    signout inactive pathologists

--- app/controllers/pathologists/sessions_controller.rb
+++ app/controllers/pathologists/sessions_controller.rb
@@ -1,28 +1,13 @@
 # frozen_string_literal: true
 
 class Pathologists::SessionsController < Devise::SessionsController
-  # include Accessible
-  # before_action :configure_sign_in_params, only: [:create]
+  after_action :authenticate_activity, only: :create
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  private
+  def authenticate_activity
+    if current_pathologist.is_inactive?
+      flash[:danger] = "Your account is currently inactive, please contact your laborarory administrator to change yous activity status"
+      sign_out current_pathologist
+    end
+  end
 end
